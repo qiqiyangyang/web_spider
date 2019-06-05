6b1f28865cb28142e45dddf40d1790fb4de3e080
qiqiyangyang <30425716+qiqiyangyang@users.noreply.github.com>
2019-06-05 09:55:44 +0800

    Update parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -9,8 +9,8 @@ func Parser(body []byte)(s []string){
     m := map[string]int{}
     reg := regexp.MustCompile(`(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]`)
     //为保证url地址不重复，使用map
-	for _, d := range reg.FindAllString(string(body), -1){
-		m[d] = 0
+    for _, d := range reg.FindAllString(string(body), -1){
+	m[d] = 0
     }
     for k, _ := range m{
         url_list = append(url_list, k)
